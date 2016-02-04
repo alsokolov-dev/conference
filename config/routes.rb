bc1008346c6e0828357ebade9b5ccc1e76634b81
Alexey <alsokolov.dev@gmail.com>
2016-02-04 23:54:48 +0300

    Adding login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   get 'users/new'
-
   get 'conference/home'
-
   get 'conference/registration'
-
   get 'conference/program'
 
   resources :users
