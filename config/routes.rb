2a653dda093aa29824703d39ea49b78a87c7655e
Alexey <alsokolov.dev@gmail.com>
2016-02-07 14:55:53 +0300

    Adding email verifications

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
@@ -8,6 +12,8 @@ Rails.application.routes.draw do
   get 'conference/program'
 
   resources :users
+  resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160207093759) do
+ActiveRecord::Schema.define(version: 20160207113529) do
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
@@ -30,6 +30,11 @@ ActiveRecord::Schema.define(version: 20160207093759) do
     t.string   "password_digest"
     t.string   "remember_digest"
     t.boolean  "admin",             default: false
+    t.string   "activation_digest"
+    t.boolean  "activated",         default: false
+    t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
